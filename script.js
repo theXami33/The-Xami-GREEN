afd691e7196d532561338fb7cebcb9b0d4c43faa
theXami33 <jesusalmazanrosales@gmail.com>
2025-10-08 12:23:16 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+apiKey: "AIzaSyBGErpLGBlank8EzXbxgMgbC23r6zagZ04",
+authDomain: "the-xami-green-ab450.firebaseapp.com",
+databaseURL: "https://the-xami-green-ab450-default-rtdb.europe-west1.firebasedatabase.app",
+projectId: "the-xami-green-ab450",
+storageBucket: "the-xami-green-ab450.firebasestorage.app",
+messagingSenderId: "441415147657",
+appId: "1:441415147657:web:3928d7dc54d3b2bf35a658"
+};
